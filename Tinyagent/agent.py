e36b0d282326531aaff00f105c786428df6515cb
Aryan Katoch <aryan.katoch@tmotions.com>
2026-09-21 18:23:40 +0530

    Add initial implementation of TinyAgent framework and requirements file

---
+++ Tinyagent/agent.py
@@ -0,0 +1,22 @@
+class TinyAgent:
+    """A minimal, modular, and educational agent framework."""
+
+    def __init__(self):
+        self.llm = None 
+        self.memory = None 
+        self.tools = None
+        self.planner = None
+
+
+    def run(self, task: str):
+        """Run agent on task"""
+        return self._step(task)
+
+    def _step(self, task: str) -> str:
+        """Run a single step """
+        return f"Received: {task}"
+
+    def _execute_action(self, action: str) -> str:
+        """Execute a tool action."""
+        return f"Executed action: {action}" 
+
